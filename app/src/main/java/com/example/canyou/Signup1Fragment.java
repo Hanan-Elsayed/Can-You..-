0a0aa7b888bf1de1088d50a237d34f14e995dd11
Hanan Elsayed <noonelsayed990@gmail.com>
2023-03-11 07:38:37 +0200

    Date Picker

--- app/src/main/java/com/example/canyou/Signup1Fragment.java
+++ app/src/main/java/com/example/canyou/Signup1Fragment.java
@@ -1,5 +1,9 @@
 package com.example.canyou;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,41 +11,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Signup1Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.time.Year;
+import java.util.Calendar;
+
+
 public class Signup1Fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Signup1Fragment() {
         // Required empty public constructor
     }
+TextInputLayout dateOfBirth;
+    TextInputEditText dateOfBirthEt;
+    DatePickerDialog.OnDateSetListener setListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Signup1Fragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static Signup1Fragment newInstance(String param1, String param2) {
         Signup1Fragment fragment = new Signup1Fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +42,7 @@ public class Signup1Fragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -59,6 +50,34 @@ public class Signup1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup1, container, false);
+        View view =inflater.inflate(R.layout.fragment_signup1, container, false);
+        dateOfBirth=view.findViewById(R.id.date_of_birth);
+        dateOfBirthEt=view.findViewById(R.id.date_of_birthET);
+        Calendar calendar =Calendar.getInstance();
+        final int year =calendar.get(calendar.YEAR);
+        final int month =calendar.get(calendar.MONTH);
+        final int day =calendar.get(calendar.DAY_OF_MONTH);
+        dateOfBirthEt.setOnClickListener(new View.OnClickListener() {
+     @Override
+     public void onClick(View v) {
+         DatePickerDialog datePickerDialog = new DatePickerDialog(
+                 getContext(), new DatePickerDialog.OnDateSetListener() {
+             @Override
+             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                 dateOfBirthEt.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+             }
+         },year,month,day);
+         datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+         calendar.add(Calendar.DATE,-7);
+         datePickerDialog.getDatePicker().setMinDate(Calendar.MILLISECOND);
+         datePickerDialog.show();
+
+     }
+ });
+
+        return view;
     }
 }
+
+
