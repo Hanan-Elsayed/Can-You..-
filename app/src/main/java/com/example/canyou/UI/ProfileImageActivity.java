711005e6fffa0eb594c4fad5bc2524d07d9d1cef
Hanan Elsayed <noonelsayed990@gmail.com>
2023-05-28 23:04:21 +0300

    enable data binding

--- app/src/main/java/com/example/canyou/UI/ProfileImageActivity.java
+++ app/src/main/java/com/example/canyou/UI/ProfileImageActivity.java
@@ -1,16 +1,19 @@
 package com.example.canyou.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.example.canyou.R;
+import com.example.canyou.databinding.ActivityLoginBinding;
+import com.example.canyou.databinding.ActivityProfileImageBinding;
 
 public class ProfileImageActivity extends AppCompatActivity {
-
+    private ActivityProfileImageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_image);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_profile_image);
     }
 }
